c8b9e9f50b6e03fdf95e83f086f7a7148ba32629
tushar087 <tushar087@gmail.com>
2025-04-03 00:51:20 +0530

    Minimal API

---
+++ DataAccess/TodoDb.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Minimal_API.Model;
+
+namespace Minimal_API.DataAccess
+{
+    public class TodoDb :DbContext
+    {
+        public TodoDb(DbContextOptions<TodoDb> options) : base(options) { }
+
+        public DbSet<ToDo> Todos => Set<ToDo>();
+    }
+}
---
+++ Model/ToDo.cs
@@ -0,0 +1,10 @@
+﻿namespace Minimal_API.Model
+{
+    public class ToDo
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        public bool IsComplete { get; set; }
+    }
+}
